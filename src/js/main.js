332600b358a91ef25c5147f4ab93f67fb266e498
Ahmed Ashraf <ahmedashraf2398@gmail.com>
2023-04-30 15:44:37 +0300

    fix bugs

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,34 @@ $(".regular").slick({
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 1000,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
 });
 // END
 
@@ -21,3 +49,23 @@ $('.one-time').slick({
     autoplaySpeed: 3000,
 });
 //END
+
+//3- Become a member Slider
+// $(".one-time ").slick({
+//     // dots: true,
+//     // centerMode: true,
+//     // arrows:false,
+//     // fade:true,
+//     // easing:'linear',
+//     // pauseOnHover:true,
+//     // pauseOnDotsHover:true,
+//     // rows:2,
+//     // touchMove:true,
+//     // slidesToShow: 1,
+//     // adaptiveHeight: true,
+//     dots: true,
+//     infinite: true,
+//     variableWidth: truem
+//     rows: 2,
+//     slidesToShow: 1,
+// });
