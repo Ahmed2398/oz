9d8a65fa81b9817e838dca2a16ea1767ffd0f492
Ahmed Ashraf <ahmedashraf2398@gmail.com>
2023-05-01 16:46:35 +0300

    fix bugs

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -29,7 +29,7 @@ const navbar= `
                 </li>
             </ul>
             <div class="d-flex">
-                <a class="nav-link navbar-text mx-4" href="contactus.html">inquire</a>
+                <a class="nav-link navbar-text " href="contactus.html">inquire</a>
                 <a class="nav-link m-auto" href="index.html">Login <img src="src/assets/images/icons/loginIcon.png"></a>
             </div>
         </div>
