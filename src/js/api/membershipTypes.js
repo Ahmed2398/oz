00065eac6cb396f29960497d51039da3931b139a
Ahmed Ashraf <ahmedashraf2398@gmail.com>
2023-05-15 16:31:45 +0300

    fix bugs

--- src/js/api/membershipTypes.js
+++ src/js/api/membershipTypes.js
@@ -1,22 +1,41 @@
-$.ajax({
-    url: "https://modest-banzai.78-141-219-156.plesk.page/api/get-site-settings",
-    type: "POST",
-    dataType: "json",
-    headers: {
-        "Access-Control-Allow-Origin":" *"
-    },
-    contentType: 'application/json; charset=utf-8',
-    data: {
-      "server_key": "64eb39980a91059d0292e98cac2f7e1b"
-    },
+// $.ajax({
+//     url: "https://modest-banzai.78-141-219-156.plesk.page/api/get-site-settings",
+//     type: "POST",
+//     dataType: "json",
+//     headers: {
+//         "Access-Control-Allow-Origin": "*",
+// },
+//     contentType: 'application/json',
+//     data: {
+//       "server_key": "64eb39980a91059d0292e98cac2f7e1b"
+//     },
+//
+//     success: function(response) {
+//         // code to execute when the request is successful
+//         console.log(response)
+//         alert('API called successfully.');
+//     },
+//     error: function(xhr, status, error) {
+//         // code to execute when the request fails
+//         alert('API called error.');
+//     }
+// });
+//
 
-    success: function(response) {
-        // code to execute when the request is successful
-        console.log(response)
-        alert('API called successfully.');
-    },
-    error: function(xhr, status, error) {
-        // code to execute when the request fails
-        alert('API called error.');
+// WARNING: For POST requests, body is set to null by browsers.
+var data = new FormData();
+data.append("server_key", "c04919f13f43b612fff3b76c5d08b2d6");
+
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+
+xhr.addEventListener("readystatechange", function() {
+    if(this.readyState === 4) {
+        console.log(this.responseText);
     }
 });
+
+xhr.open("POST", "https://modest-banzai.78-141-219-156.plesk.page/api/get-site-settings");
+
+xhr.send(data);
--- src/js/carousel/customSlider.js
+++ src/js/carousel/customSlider.js
@@ -1,79 +1,79 @@
-// 1- Our Membership Types Slider
-$(".regular").slick({
-    dots: false,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 100000,
-    adaptiveHeight: true,
-    responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: true
-            }
-        },
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-    ]
-});
-// END
-
-// 2- Our Locations Slider
-$('.one-time').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    adaptiveHeight: true,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 3000,
-});
-//END
-
-//3- Become a member Slider
-// $(".one-time ").slick({
-//     // dots: true,
-//     // centerMode: true,
-//     // arrows:false,
-//     // fade:true,
-//     // easing:'linear',
-//     // pauseOnHover:true,
-//     // pauseOnDotsHover:true,
-//     // rows:2,
-//     // touchMove:true,
-//     // slidesToShow: 1,
-//     // adaptiveHeight: true,
+// // 1- Our Membership Types Slider
+// $(".regular").slick({
+//     dots: false,
+//     infinite: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 100000,
+//     adaptiveHeight: true,
+//     responsive: [
+//         {
+//             breakpoint: 1024,
+//             settings: {
+//                 slidesToShow: 3,
+//                 slidesToScroll: 3,
+//                 infinite: true,
+//                 dots: true
+//             }
+//         },
+//         {
+//             breakpoint: 768,
+//             settings: {
+//                 slidesToShow: 2,
+//                 slidesToScroll: 2
+//             }
+//         },
+//         {
+//             breakpoint: 600,
+//             settings: {
+//                 slidesToShow: 2,
+//                 slidesToScroll: 2
+//             }
+//         },
+//         {
+//             breakpoint: 480,
+//             settings: {
+//                 slidesToShow: 1,
+//                 slidesToScroll: 1
+//             }
+//         }
+//         // You can unslick at a given breakpoint now by adding:
+//         // settings: "unslick"
+//         // instead of a settings object
+//     ]
+// });
+// // END
+//
+// // 2- Our Locations Slider
+// $('.one-time').slick({
 //     dots: true,
 //     infinite: true,
-//     variableWidth: truem
-//     rows: 2,
+//     speed: 300,
 //     slidesToShow: 1,
+//     adaptiveHeight: true,
+//     arrows: false,
+//     autoplay: true,
+//     autoplaySpeed: 3000,
 // });
+// //END
+//
+// //3- Become a member Slider
+// // $(".one-time ").slick({
+// //     // dots: true,
+// //     // centerMode: true,
+// //     // arrows:false,
+// //     // fade:true,
+// //     // easing:'linear',
+// //     // pauseOnHover:true,
+// //     // pauseOnDotsHover:true,
+// //     // rows:2,
+// //     // touchMove:true,
+// //     // slidesToShow: 1,
+// //     // adaptiveHeight: true,
+// //     dots: true,
+// //     infinite: true,
+// //     variableWidth: truem
+// //     rows: 2,
+// //     slidesToShow: 1,
+// // });
